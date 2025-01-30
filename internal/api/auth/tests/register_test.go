f2e4914f025f8a8e32e8d3d105cb7abd66cdbd8d
um3ra <ereakvlad3@gmail.com>
2025-01-30 14:53:29 +0200

    TEST: auth handler -> add unit tests (success case)

--- internal/api/auth/tests/register_test.go
+++ internal/api/auth/tests/register_test.go
@@ -7,46 +7,56 @@ import (
 	fake "github.com/brianvoe/gofakeit/v7"
 	"github.com/stretchr/testify/require"
 	"github.com/um3ra/auth-microservice/internal/api/auth"
+	"github.com/um3ra/auth-microservice/internal/model"
 	authMockSrv "github.com/um3ra/auth-microservice/internal/service/mocks"
 	authGrpc "github.com/um3ra/auth-microservice/pkg/auth_v1"
 )
 
-func TestHandler_login(t *testing.T) {
-	type mockBehavior func(authService *authMockSrv.AuthService)
+func TestHandler_register(t *testing.T) {
+
 	type args struct {
 		ctx context.Context
-		req *authGrpc.LoginRequest
+		req *authGrpc.RegisterRequest
 	}
+	type mockBehavior func (service *authMockSrv.AuthService)
 
 	var (
 		ctx      = context.Background()
+		name = fake.Name()
 		email    = fake.Email()
 		password = fake.Animal()
 		token    = fake.BeerName()
 	)
+	user := model.User{
+		Name: name,
+		Email: email,
+		Password: password,
+	}
 
-	testTable := []struct {
-		name         string
-		args         args
-		want         *authGrpc.LoginResponse
-		err          error
+	testTable := []struct{
+		name string
+		want *authGrpc.RegisterResponse
+		args args
+		err error
 		mockBehavior mockBehavior
 	}{
 		{
-			name: "handler login success",
-			args: args{
-				ctx: ctx,
-				req: &authGrpc.LoginRequest{
-					Email:    email,
-					Password: password,
-				},
-			},
-			want: &authGrpc.LoginResponse{
+			name: "handler register success case",
+			want: &authGrpc.RegisterResponse{
 				Token: token,
 			},
+			args: args{
+				ctx: ctx,
+				req: &authGrpc.RegisterRequest{
+					Name: name,
+					Email: email,
+					Password: password,
+					ConfirmPassword: password,
+				},
+			},
 			err: nil,
-			mockBehavior: func(authService *authMockSrv.AuthService) {
-				authService.EXPECT().Login(ctx, email, password).Return(token, nil).Once()
+			mockBehavior: func(service *authMockSrv.AuthService) {
+				service.EXPECT().Register(ctx, &user).Return(token, nil).Once()
 			},
 		},
 	}
@@ -56,7 +66,7 @@ func TestHandler_login(t *testing.T) {
 			authService := authMockSrv.NewAuthService(t)
 			tt.mockBehavior(authService)
 			handler := auth.NewAuthHandler(authService)
-			res, err := handler.Login(tt.args.ctx, tt.args.req)
+			res, err := handler.Register(tt.args.ctx, tt.args.req)
 			require.Equal(t, tt.want, res)
 			require.Equal(t, tt.err, err)
 		})
